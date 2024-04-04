a9b0c4a1e4d4f4515e21312e44e54c16e8d88f55
CodingKrist <122282123+CodingKrist@users.noreply.github.com>
2024-04-04 18:50:50 +0200

    adding profile section

--- routes/main.js
+++ routes/main.js
@@ -7,7 +7,7 @@ const { ensureAuth } = require("../middleware/auth");
 
 
 router.get('/', homeController.getIndex)
-//router.get("/profile", ensureAuth, postsController.getProfile);
+router.get("/profile", ensureAuth, postsController.getProfile);
 router.get("/feed", ensureAuth, postsController.getFeed);
 router.get('/login', authController.getLogin)
 router.post('/login', authController.postLogin)
